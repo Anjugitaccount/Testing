9b25c4bc86c9dfc526afdf31494c6600a7765845
anju <anju@cit-lp-032>
2023-09-11 21:10:16 +0530

    Testing

---
+++ Maven_sample_project/src/main/java/newpackage/NewClass.java
@@ -0,0 +1,22 @@
+package newpackage;
+
+import java.util.*;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the string value");
+String b=sc.nextLine();
+
+System.out.println("Enter the number");
+int a=sc.nextInt();
+
+System.out.println("Enter the char value");
+char c=sc.next().charAt(0);
+
+
+	}
+
+}
